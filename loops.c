5fa20ee006145224bebb506b209deae712186c61
Mr-awesom <alidaryabqasemi777@gmail.com>
2026-02-12 11:41:30 +0200

    making a simple void some_function(void) function

--- loops.c
+++ loops.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 
+void meow(void);
+
 int main(void)
 {
     int i = 0;
     while ( i < 3 )
     {
-        printf("Meow\n");
+        printf("Moooooo\n");
         i++;
     }
 
     for (int i = 0; i < 3; i++)
     {
-        printf("Woof wit for loop\n");
+        printf("Woof with for loop\n");
     }
 
+    for (int i = 0; i < 3; i++)
+    {       
+    meow();
+    }
+
+}
+
+void meow (void)
+{
+    printf("Meow\n");
 }
