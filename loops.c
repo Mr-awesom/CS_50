f7891aad2fe3ce411eb03a7cce0bb0ce70ea571d
Mr-awesom <alidaryabqasemi777@gmail.com>
2026-02-12 11:32:46 +0200

    Simple loops

---
+++ loops.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 0;
+    while ( i < 3 )
+    {
+        printf("Meow\n");
+        i++;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Woof wit for loop\n");
+    }
+
+}
