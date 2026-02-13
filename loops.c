d1effbb81cf01931cbecc1a34c6da314dc188e34
Ali Qasemi <alidaryabqasemi777@gmail.com>
2026-02-13 11:15:51 +0200

    Every changes made so far

--- loops.c
+++ loops.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 void meow(void);
-
+void oink(int n);
 int main(void)
 {
-    int i = 0;
+    /*int i = 0;
     while ( i < 3 )
     {
         printf("Moooooo\n");
@@ -14,16 +14,33 @@ int main(void)
     for (int i = 0; i < 3; i++)
     {
         printf("Woof with for loop\n");
-    }
+    }*/
 
-    for (int i = 0; i < 3; i++)
+    /*for (int i = 0; i < 3; i++)
     {       
     meow();
+    }*/
+    int number;
+    
+    do 
+    {
+        printf("How many times do you want to oink? ");
+        scanf("%d", &number);
     }
+    while (number < 1);
+    oink(number);
 
 }
 
 void meow (void)
 {
     printf("Meow\n");
+}
+
+void oink(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("Oink\n");
+    }
 }
