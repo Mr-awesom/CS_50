471d740e4dca841f29b56f5d931775cd945daf5c
Ali Qasemi <alidaryabqasemi777@gmail.com>
2026-03-04 17:00:43 +0200

    added height

--- buggy.c
+++ buggy.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdint.h>
 
+void print_column(int height);
+
+
 int main()
 {
-    for (uint8_t i = 0; i<3; i++)
-    {
-        printf("i is %i\n", i);
-        printf("#\n");
-    }
+  int h;
+  printf("Enter height: \n");
+  scanf("%d", &h);
+  print_column(h);
+}
+
+void print_column(int height)
+{
+  for (int i = 0; i <= height; i++)
+  {
+    printf("#\n");
+  }
 }
